befee589e1152979742d75a8e5bdc71342cdb82c
Raul Cayo <raul.sanchez.cayo@gmail.com>
2018-12-22 09:44:41 -0600

    Returning JSON

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ let server = http.createServer(function(req, res){
 			let payloadString = JSON.stringify(payload);
 
 			// Return the response
+			res.setHeader('Content-Type', 'application/json');
 			res.writeHead(statusCode);
 			res.end(payloadString);
 
